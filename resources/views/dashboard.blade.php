396db3f7ec50c68200b8acf678562c4544100f9b
fukushigetomoya <t.fukushige.24.3.1302@4nonome.com>
2024-09-13 09:35:57 +0900

    コンポーネントを利用してボタン追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,12 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <x-primary-button>
+                        ボタン
+                    </x-primary-button>
+                    <x-primary-button>
+                        ok
+                    </x-primary-button>
                 </div>
             </div>
         </div>
