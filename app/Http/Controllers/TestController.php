adf898da3309679548022ce97a4ed91d7e60238d
fukushigetomoya <t.fukushige.24.3.1302@4nonome.com>
2024-09-14 09:20:03 +0900

    データを取得する処理の追加

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class TestController extends Controller
 {
     public function test(){
+        // usersテーブルに入っているレコードをすべて取得
+        $users = User::all();
         // resoursesのviewsにあるtest.blade.phpを表示
-        return view('test');
+        return view('test',compact('users'));
     }
 }
