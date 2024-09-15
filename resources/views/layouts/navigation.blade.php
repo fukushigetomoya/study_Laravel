efb1aace0b3513c992ddd5564f9010b5359c9545
fukushigetomoya <t.fukushige.24.3.1302@4nonome.com>
2024-09-16 08:41:51 +0900

    HomeとPostのナビゲーション追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('post.create')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('post.index')" :active="request()->routeIs('post.index')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('post.create')" :active="request()->routeIs('post.create')">
                         {{ __('Post') }}
                     </x-nav-link>
                 </div>
